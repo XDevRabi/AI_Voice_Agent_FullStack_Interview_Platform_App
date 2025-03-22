0baea72edde5b214921f047d9e702dc73c41d36e
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-03-22 18:50:22 +0545

    feat: add utility functions, type definitions, and constants
    Introduce new utility functions for handling tech logos and interview covers, define essential types for the application, and add constants for mappings and interview covers to improve code organization and maintainability.

---
+++ lib/utils.ts
@@ -0,0 +1,53 @@
+import { interviewCovers, mappings } from "@/constants";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// Returns the tech logo url for the given tech name
+const techIconBaseURL = "https://cdn.jsdelivr.net/gh/devicons/devicon/icons";
+
+// Normalizes the tech name using constact file's mapping variable to match the devicon names
+const normalizeTechName = (tech: string) => {
+  const key = tech.toLowerCase().replace(/\.js$/, "").replace(/\s+/g, "");
+  return mappings[key as keyof typeof mappings];
+};
+
+const checkIconExists = async (url: string) => {
+  try {
+    const response = await fetch(url, { method: "HEAD" });
+    return response.ok; // Returns true if the icon exists
+  } catch {
+    return false;
+  }
+};
+
+// Returns an array of objects with the tech name and the url of the icon
+// If the icon does not exist, it returns the default icon
+export const getTechLogos = async (techArray: string[]) => {
+  const logoURLs = techArray.map((tech) => {
+    const normalized = normalizeTechName(tech);
+    return {
+      tech,
+      url: `${techIconBaseURL}/${normalized}/${normalized}-original.svg`,
+    };
+  });
+
+  const results = await Promise.all(
+    logoURLs.map(async ({ tech, url }) => ({
+      tech,
+      url: (await checkIconExists(url)) ? url : "/tech.svg",
+    }))
+  );
+
+  return results;
+};
+
+// Returns a random interview cover from the interviewCovers array
+// Used in the /interview page to display a random interview cover
+export const getRandomInterviewCover = () => {
+  const randomIndex = Math.floor(Math.random() * interviewCovers.length);
+  return `/covers${interviewCovers[randomIndex]}`;
+};
