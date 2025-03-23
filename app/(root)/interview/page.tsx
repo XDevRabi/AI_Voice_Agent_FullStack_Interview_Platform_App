302da585a0ce116fa47474b25065973e6f506410
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-03-23 09:49:26 +0545

    feat(interview): add interview generation page with user details
    Introduce a new page for interview generation that fetches and displays user details. The page utilizes the `Agent` component to handle the interview generation process based on the user's information.

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  return (
+    <>
+      <h3>Interview generation</h3>
+
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
